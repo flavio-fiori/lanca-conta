5c3423d9238f1dff27b4278a2ad598585fc1087d
Flávio <flavio.fiori31@gmail.com>
2021-08-15 14:46:49 -0300

    :rocket: feat(project/layout): add home screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ResumeComponent } from './resume/resume.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ResumeComponent,
+    TransactionsComponent,
   ],
   imports: [
     BrowserModule,
